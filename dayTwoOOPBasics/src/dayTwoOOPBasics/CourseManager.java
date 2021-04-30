a8d52932b213dd5b4916374f7db4f52298ef0936
sametyabalak <sametyabalak@outlook.com>
2021-05-01 01:15:55 +0300

    courseManager has been created

---
+++ dayTwoOOPBasics/src/dayTwoOOPBasics/CourseManager.java
@@ -0,0 +1,11 @@
+package dayTwoOOPBasics;
+
+public class CourseManager {
+	public void addToBasket(Course course) {
+		System.out.println("The product with an ID of " + course.courseId + " has been added to the basket.");
+	}
+
+	public void deleteFromBasket(Course course) {
+		System.out.println("The product with an ID of " + course.courseId + " has been deleted from the basket.")
+	}
+}
--- dayTwoOOPBasics/src/dayTwoOOPBasics/Main.java
+++ dayTwoOOPBasics/src/dayTwoOOPBasics/Main.java
@@ -4,7 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello oop");
+
+		// default constructor instance
+		Course firstCourse = new Course();
+		firstCourse.courseId = 1;
+		firstCourse.courseName = "Java & React Bootcamp";
+		firstCourse.instructorName = "Engin Demirog";
+
+		// constructor with parameters
+		Course secondCourse = new Course(2, "C# BootCamp", "Engin Demirog");
+
+		Course[] courses = { firstCourse, secondCourse };
+
+		for (Course course : courses) {
+			System.out.println(course.courseName + "\n" + course.instructorName + "\n---");
+		}
 	}
 
 }
